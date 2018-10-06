7161892250bbd39da2ea514ca14dd506dcf748a1
steven real sanchez <stevenn9549@gmail.com>
2018-10-06 00:14:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function happyHolidayTo(holiday, name){
   return 'Happy Independence Day, you!'
 }
 function holidayCountdown(holiday, days){
-  return 'it's
+  return 'it\'s 20 days until Mother\'s day!'
 }
